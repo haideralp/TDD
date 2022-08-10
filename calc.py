9ddaf164cee080daf41c5869a3e8aa3779143b4a
Syed Haider Abedi <haider.abedi@gmx.com>
2022-08-10 15:28:26 +0100

    update

--- calc.py
+++ calc.py
@@ -12,3 +12,15 @@ class BasicCalc:
     def divide(self, value1, value2):
         return value1 / value2
 
+    def dob(self, date, month):
+        if date < 1:
+            x_1 = int(input("Enter a date. "))
+            x_2 = int(input("Enter a month. "))
+            date, month = dob(x_1, x_2)
+        return date, month
+    def percentage(self, x1, x2):
+        if x1 | x2 == 0:
+            x1_1 = int(input("Enter a new number. "))
+            x2_2 = int(input("Enter a new number. "))
+            percentage(x1_1, x2_2)
+        return 100 * x1 / x2 
--- calc_tests.py
+++ calc_tests.py
@@ -15,3 +15,9 @@ class Calc_tests(unittest.TestCase):
 
     def test_divide(self):
         self.assertEqual(self.calc_obj.divide(10, 5), 2)
+
+    def test_dob(self):
+        self.assertEqual(self.calc_obj.dob(7,12),7,12)
+
+    def test_percentage(self):
+        self.assertEqual((self.calc_obj.percentage(7,8),87.5)
