6372ad260f0b1e80651c23596dcaed321cb716eb
Syed Haider Abedi <haider.abedi@gmx.com>
2022-08-10 14:40:38 +0100

    update

---
+++ calc.py
@@ -0,0 +1,14 @@
+class Calc:
+
+    def add(self, value1, value2):
+        return value1 + value2
+
+    def subtract(self, value1, value2):
+        return value1 - value2
+
+    def multiply(self, value1, value2):
+        return value1 * value2
+
+    def divide(self, value1, value2):
+        return value1 / value2
+
