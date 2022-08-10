96290e9e7476e8f6d7e81a22a06e7912269a170d
Syed Haider Abedi <haider.abedi@gmx.com>
2022-08-10 17:41:38 +0100

    updated - fixed errors

--- calc.py
+++ calc.py
@@ -17,12 +17,11 @@ class BasicCalc:
 # Create Percentage function
 # Create cm to m function
 # Create modulo function
-
-    def DOB(self,value1,value2):
-        return (f"Your DOB is {value1} / {value2}")
+    def dob(self,value1,value2):
+        return f"Your DOB is {value1} / {value2}"
     def percentage(self, value1, value2):
         return value1/value2 * 100
     def conversion(self, value):
         return value/100
     def modulo(self, value1, value2):
-        return value1%value2
+        return value1 % value2
